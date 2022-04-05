fd94f9d48ddcff4a9b204fc3ac1b869f8b93f130
janican <nieminenjanica@gmail.com>
2022-04-05 12:06:50 +0300

    edit

---
+++ src/main/java/app/model/EditCandidates.java
@@ -0,0 +1,79 @@
+package app.model;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(
+		name = "EditCandidates",
+		urlPatterns = {"/editcandidates"}
+		)
+
+public class EditCandidates extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws IOException, ServletException {
+		
+		
+		// if sessions does not exist, create new one
+		HttpSession session = request.getSession();
+		
+		String idValue = request.getParameter("id");
+		
+		if ( idValue != null ) {
+			try {
+				int id = Integer.parseInt(idValue);
+				
+				Dao dao = new Dao();
+				Candidates candidates = dao.getCandidatesInfo(id);
+				
+				session.setAttribute("candidates", candidates);
+				
+				RequestDispatcher rd = request.getRequestDispatcher("jsp/editform.jsp");
+				rd.forward(request, response);
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			// Back to list
+			response.sendRedirect("/showdata");
+			
+		}
+	
+	}
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws IOException, ServletException {
+	
+		
+		// Create connection
+		Dao dao=new Dao();
+		Candidates candidates = readCandidates(request);
+		
+		dao.updateCandidates(candidates);
+		
+		dao.close();
+		
+		// Back to list after actions
+		//RequestDispatcher rd = request.getRequestDispatcher("/showdata");
+		//rd.forward(request, response);
+		response.sendRedirect("/showdata");
+	}
+	
+	private Candidates readCandidates(HttpServletRequest request) {
+		Candidates candidates=new Candidates();
+		candidates.setSukunimi(request.getParameter("sukunimi"));
+		candidates.setEtunimi(request.getParameter("etunimi"));
+		candidates.setId(Integer.parseInt(request.getParameter("id")));
+		return candidates;
+	}
+}
