8bb050f100187c3ca751d1ba203114d25dffc650
CariRyt <90597943+CariRyt@users.noreply.github.com>
2022-04-07 15:13:54 +0300

    jeejee

---
+++ src/main/java/app/model/SecurityUtils.java
@@ -0,0 +1,36 @@
+package app.model;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class SecurityUtils {
+		
+		public static String getPasswordHashed(String HashSalasana, String salt) {
+			String result = "";
+			
+			try {
+				MessageDigest md = MessageDigest.getInstance("SHA-256");
+				
+				byte[] saltBytes = Base64.getDecoder().decode(salt);
+				md.update(saltBytes);
+				byte[] bytes = md.digest(HashSalasana.getBytes());
+				
+				result = Base64.getEncoder().encodeToString(bytes);
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			}
+			return result;
+		}
+
+
+		public static String getSalt() {
+			String result = "";
+			SecureRandom rd = new SecureRandom();
+			byte[] salt = new byte[16];
+			rd.nextBytes(salt);
+			result = Base64.getEncoder().encodeToString(salt);
+			return result;
+		}
+}
