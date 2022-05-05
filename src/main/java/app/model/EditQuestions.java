7e6a7da70c3e495dcf48d3bf2b8bb6c34bf8a345
Carita Rytkölä <carita.rytkola@student.hamk.fi>
2022-05-05 11:35:54 +0300

    error fixu

--- src/main/java/app/model/EditQuestions.java
+++ src/main/java/app/model/EditQuestions.java
@@ -2,6 +2,7 @@ package app.model;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
